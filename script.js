8105ba064e3c8942cb72e7201890f955d36002ca
Dewan Mahmud Rocky <rockyit10@gmail.com>
2025-10-27 14:13:20 -0400

    Task_1_Update_CSS_and_Console.logs

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// javaScript is wild wild west no rules but best practices.
+//mdn and w3schools web docs is my go to reference for best practices and methods
+//Task1 formatFullName
 function formatFullName(firstName, lastName) {
 
     if (!firstName) {
@@ -7,20 +10,21 @@ function formatFullName(firstName, lastName) {
     if (!lastName) {
         return "Invalid name input.";
     }
-
 // capitalize first letter not alphabet of each name
-   
-firstName = firstName.charAt(0).toUpperCase() + 
-    
-    firstName.slice(1).toLowerCase();
-    
-    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase(); //slice method to get rest of the string after first character
-
+    firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase(); //slice mdn
+    lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1).toLowerCase(); //slice mdn
     return lastName + ", " + firstName;
-  
     // References: practiced from MDN Web Docs (practiced methods) practice makes perfect
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/charAt
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toLowerCase
     // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/toUpperCase
-}
+} 
+// we can comment out the below console logs to see the output in the browser console
+
+console.log(formatFullName("Trainor", "Tishana"));    // Tishana, Trainor my teacher at Per Scholas
+console.log(formatFullName("Santos", "Bryan"));      // Bryan Santos
+
+console.log(formatFullName("dewan", "mahmud")); // Mahmud, Dewan student at Per Scholas
+console.log(formatFullName("", "mahmud"));      // Invalid name input. because first name is missing
+console.log(formatFullName("rocky"));           // Invalid name input. because last name is missing
